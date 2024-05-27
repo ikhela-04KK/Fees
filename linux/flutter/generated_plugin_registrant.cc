6710bd4186b3194449d23f11bf331a48e8291620
Marc koffi <102194271+ikhela-04KK@users.noreply.github.com>
2024-05-27 00:51:11 +0000

    add a new packages

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,14 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <awesome_notifications/awesome_notifications_plugin.h>
+#include <webcrypto/webcrypto_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) awesome_notifications_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "AwesomeNotificationsPlugin");
+  awesome_notifications_plugin_register_with_registrar(awesome_notifications_registrar);
+  g_autoptr(FlPluginRegistrar) webcrypto_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "WebcryptoPlugin");
+  webcrypto_plugin_register_with_registrar(webcrypto_registrar);
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import awesome_notifications
+import shared_preferences_foundation
+import webcrypto
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WebcryptoPlugin.register(with: registry.registrar(forPlugin: "WebcryptoPlugin"))
 }
