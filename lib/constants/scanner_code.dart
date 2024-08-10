c1fb33f71b86e461d2abd36741c9f1a492c27ea0
Marc koffi <102194271+ikhela-04KK@users.noreply.github.com>
2024-08-10 01:27:41 +0000

    update mobile scanner

--- lib/constants/scanner_code.dart
+++ lib/constants/scanner_code.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:provider/provider.dart';
+import 'package:fees/components/animation/loading_provider.dart';
 
 class ScannerCode extends StatefulWidget {
   const ScannerCode({super.key});
@@ -9,15 +11,15 @@ class ScannerCode extends StatefulWidget {
 }
 
 class _ScannerCodeState extends State<ScannerCode> {
-  final MobileScannerController controller = MobileScannerController();
+   final MobileScannerController cameraController = MobileScannerController(formats: [BarcodeFormat.qrCode]);
 
-  bool _screenOpened = false; 
   @override
   void initState(){
     super.initState(); 
   }
   @override
   Widget build(BuildContext context) {
+    final loadingProvider = Provider.of<LoadingProvider>(context);
     return Scaffold(
       backgroundColor: Colors.black.withOpacity(0.5),
       appBar: AppBar(
@@ -29,14 +31,45 @@ class _ScannerCodeState extends State<ScannerCode> {
         child: SizedBox(
           height: 400,
           child: MobileScanner(
+            controller: cameraController,
+            overlayBuilder: _buildOverlay,
             onDetect: (capture) {
             final List<Barcode> barcodes = capture.barcodes;
             for (final barcode in barcodes) {
               Navigator.canPop(context) ? Navigator.pop<String>(context, barcode.rawValue ?? 'No data in QR') : null;
             }
-          }),
+          },
+          
+          ),
             ),
       )
+    );
+  }
+
+    Widget _buildOverlay(BuildContext context, BoxConstraints constraints) {
+    return Stack(
+      children: [
+        _buildAlignedScanQrHint(),
+      ],
+    );
+  }
+
+   Widget _buildAlignedScanQrHint() {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        margin: EdgeInsets.only(top: 20),
+        color: Colors.white,
+        child:Text(
+          "QrCode",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.red
+          ),
+        ),
+      ),
     );
   }
 
